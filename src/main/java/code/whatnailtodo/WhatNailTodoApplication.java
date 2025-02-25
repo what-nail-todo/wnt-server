c164126229b454e80677a8adbec78eeb009a51d6
xunxxoie <ahh0520@naver.com>
2025-02-25 21:28:02 +0900

    [Feat] Initial Commit

---
+++ src/main/java/code/whatnailtodo/WhatNailTodoApplication.java
@@ -0,0 +1,13 @@
+package code.whatnailtodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatNailTodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatNailTodoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/code/whatnailtodo/WhatNailTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package code.whatnailtodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatNailTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
