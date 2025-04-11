e69435f9f2600000107ad1f9a1ae403a4b3b7692
xunxxoie <ahh0520@naver.com>
2025-04-12 01:35:00 +0900

    [Feat] oauth SecurityConfig 추가 - #9

--- src/main/java/code/global/config/SecurityConfig.java
+++ src/main/java/code/global/config/SecurityConfig.java
@@ -1,7 +1,12 @@
 package code.global.config;
 
+import code.domain.oauth.service.CustomOAuth2UserService;
+import code.domain.oauth.util.OAuth2AuthorizationRequestRepository;
 import code.global.exception.handler.AccessDeniedHandlerImpl;
 import code.global.exception.handler.AuthenticationEntryPointImpl;
+import code.global.exception.handler.OAuth2AuthenticationFailureHandler;
+import code.global.exception.handler.OAuth2AuthenticationSuccessHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,7 +24,13 @@ import java.util.List;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+    private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
+    private final OAuth2AuthorizationRequestRepository oAuth2AuthorizationRequestRepository;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
@@ -32,6 +43,12 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2Login(configuration -> configuration
+                        .authorizationEndpoint(config -> config.authorizationRequestRepository(oAuth2AuthorizationRequestRepository))
+                        .userInfoEndpoint(config -> config.userService(customOAuth2UserService))
+                        .successHandler(oAuth2AuthenticationSuccessHandler)
+                        .failureHandler(oAuth2AuthenticationFailureHandler)
+                )
                 .exceptionHandling(handler -> handler
                         .authenticationEntryPoint(new AuthenticationEntryPointImpl())
                         .accessDeniedHandler(new AccessDeniedHandlerImpl())
