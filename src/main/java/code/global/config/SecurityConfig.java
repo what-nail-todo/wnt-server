191d975161b712e4c6e8e8c03e0a59ca600e4a25
xunxxoie <ahh0520@naver.com>
2025-05-06 22:40:18 +0900

    [Fix] CORS 도메인 변경

--- src/main/java/code/global/config/SecurityConfig.java
+++ src/main/java/code/global/config/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOriginPatterns(List.of("https://what-nail-todo.inuappcenter.kr"));
+        configuration.setAllowedOriginPatterns(List.of("https://what-nail-todo.inuappcenter.kr:8520"));
         configuration.setExposedHeaders(List.of("*"));
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
